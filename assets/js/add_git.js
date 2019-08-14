6a13fa2c0fed873369742e1ea67e799caed6433f
WStrellis <wstrellis@gmail.com>
2019-08-14 09:40:12 -0400

    put api request in seperate function

--- assets/js/add_git.js
+++ assets/js/add_git.js
@@ -1,26 +1,39 @@
 'use strict'
 
-const makeRepoCard = ( url, name) =>{
-  let sec = document.createElement('section');
-  sec.classList.add('col-6', 'col-12-narrower', 'feature');
-  sec.insertAdjacentHTML( 'afterbegin', `<header><h2>${name}</h2></header><ul class="actions"><li><a href=${url} target="_blank" class="button">View on Github</a></li></ul></section>`)
+const makeRepoCard = ( { html_url, name } ) =>{
+  /* create a card */
+  let section = document.createElement('section');
+  
+  section.classList.add('col-6', 'col-12-narrower', 'feature');
 
-  document.querySelector('div.row.gitRepos').appendChild(sec);
+  section.insertAdjacentHTML( 'afterbegin', `<header><h2>${name}</h2></header><ul class="actions"><li><a href=${html_url} target="_blank" class="button">View on Github</a></li></ul></section>`)
+
+  return section;
+}
+
+const makeRepoList = ( apiURL) =>{
+
+  let parent = document.querySelector('div.row.gitRepos');
+
+   fetchData( apiURL).then(
+    data => {
+        // make list of repos
+        data.forEach( obj => parent.appendChild(makeRepoCard( obj )));
+    }
+   )
+    // do nothing if there is an error with the api request
+    .catch( err => false)
+}
+
+const fetchData = async ( apiURL ) =>{
+  // fetch data from source
+  let response = await fetch( apiURL);
+  // convert data to JSON and return it
+  return await response.json();
 }
 
 
 
-const makeRepoList = () =>{
-  fetch("https://api.github.com/users/WStrellis/repos")
-    .then(response => response.json() )
-    .then(  resData => {
-      // make list of repos
-      resData.forEach( obj => makeRepoCard( obj.html_url, obj.name))
-    })
-    .catch(err => false)
-    
-}
-
-window.addEventListener("load", (e)=>{
-  makeRepoList()
+window.addEventListener("load", ()=>{
+  makeRepoList("https://api.github.com/users/WStrellis/repos");
 }, false)
